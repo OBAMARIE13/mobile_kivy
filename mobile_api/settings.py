2d4483a39fad323ed9cdd7d1618c46bd66f335cf
OBAMARIE <obaouattara@gmail.com>
2021-09-02 15:31:02 +0000

    christelle

--- mobile_api/settings.py
+++ mobile_api/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-+a6n*3&=%a(3trw*y#@ldm=%=94*mi!qd9tigs_(#h&6=uer-!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mobilekivyapp.herokuapp.com']
 
 
 # Application definition
@@ -121,6 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Default primary key field type
